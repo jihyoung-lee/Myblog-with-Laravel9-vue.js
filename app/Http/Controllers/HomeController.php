f5ac0ad36d0d08a605391f015b4b7dfac17163a2
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2023-03-01 15:08:30 +0900

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+
+class HomeController extends Controller
+{
+ public function index(){
+    return PostResource::collection(Post::latest()->take(4)->get());
+ }
+}
