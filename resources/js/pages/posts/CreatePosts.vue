7c1680c475656c5c13e664fbddbe81caec2913c8
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2023-03-07 13:49:23 +0900

    Update CreatePosts.vue

--- resources/js/pages/posts/CreatePosts.vue
+++ resources/js/pages/posts/CreatePosts.vue
@@ -1,110 +1,90 @@
 <template>
-    <main class="create-post">
-      <div class="container">
-        <h1>Create Posts!</h1>
-        <!-- success message -->
-        <div class="success-msg" v-if="success">
-          <i class="fa fa-check"></i>
-          Post created successfully
-        </div>
-        <!-- Contact Form -->
-        <div class="contact-form">
-          <form @submit.prevent="submit">
-            <!-- Title -->
-            <label for="title"><span>Title</span></label>
-            <input type="text" id="title" v-model="fields.title" />
-            <span v-if="errors.title" class="error">{{ errors.title[0] }}</span>
-            <br />
-
-            <!-- Image -->
-            <label for="image"><span>Image</span></label>
-            <input type="file" id="image" @input="grabFile" />
-            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
-            <div class="preview">
-              <img :src="url" alt="" />
-            </div>
-            <br />
-
-            <!-- Drop down -->
-            <label for="categories"><span>Choose a category:</span></label>
-            <select v-model="fields.category_id" id="categories">
-              <option disabled value="">Select option</option>
-              <option
-                :value="category.id"
-                v-for="category in categories"
-                :key="category.id"
-              >
-                {{ category.name }}
-              </option>
-            </select>
-            <span v-if="errors.category_id" class="error">{{
-              errors.category_id[0]
-            }}</span>
-            <br />
-
-            <!-- Body-->
-            <label for="body"><span>Body</span></label>
-            <textarea id="body" v-model="fields.body"></textarea>
-            <span v-if="errors.body" class="error">{{ errors.body[0] }}</span>
-            <!-- Button -->
-            <input class="add-post-btn" type="submit" value="Submit" />
-          </form>
-        </div>
+  <main class="create-post">
+    <div class="container">
+      <h1>Create Posts!</h1>
+      <div class="success-msg" v-if="success">
+        <i class="fa fa-check"></i>
+        Post created successfully
       </div>
-    </main>
-  </template>
+      <div class="contact-form">
+        <form @submit.prevent="submit">
+          <label for="title"><span>Title</span></label>
+          <input type="text" id="title" v-model="fields.title" />
+          <span v-if="errors.title" class="error">{{ errors.title[0] }}</span>
+           <label for="image"><span>Image</span></label>
+      <input type="file" id="image" @change="grabFile" />
+      <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+      <div class="preview" v-if="url">
+        <img :src="url" alt="" />
+      </div>
 
-  <script>
-  export default {
-    data() {
-      return {
-        success: false,
-        fields: {
-          category_id: "",
-        },
-        errors: {},
-        url: "",
-        categories: [],
-      };
-    },
-    methods: {
-      grabFile(e) {
-        const file = e.target.files[0];
-        this.fields.file = file;
-        this.url = URL.createObjectURL(file);
-        URL.revokeObjectURL(file);
+      <label for="categories"><span>Choose a category:</span></label>
+      <select v-model="fields.category_id" id="categories">
+        <option disabled value="">Select option</option>
+        <option :value="category.id" v-for="category in categories" :key="category.id">{{ category.name }}</option>
+      </select>
+      <span v-if="errors.category_id" class="error">{{ errors.category_id[0] }}</span>
+
+      <label for="body"><span>Body</span></label>
+      <textarea id="body" v-model="fields.body"></textarea>
+      <span v-if="errors.body" class="error">{{ errors.body[0] }}</span>
+
+      <button class="add-post-btn" type="submit">Submit</button>
+    </form>
+  </div>
+</div>
+ </main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      success: false,
+      fields: {
+        category_id: "",
       },
-      submit() {
-        axios
-          .post("/api/posts", this.fields, {
-            headers: { "content-type": "multipart/form-data" },
-          })
-          .then(() => {
-            this.fields = {};
-            this.url = null;
-            this.fields.category_id = "";
-            this.success = true;
-            this.errors = {};
-            setTimeout(() => {
-              this.success = false;
-            }, 2500);
-          })
-          .catch((error) => {
-            this.errors = error.response.data.errors;
-            this.success = false;
-          });
-      },
+      errors: {},
+      url: null,
+      categories: [],
+    };
+  },
+  methods: {
+    grabFile(e) {
+      const file = e.target.files[0];
+      this.fields.file = file;
+      this.url = URL.createObjectURL(file);
     },
-    mounted() {
-      axios
-        .get("/api/categories")
-        .then((response) => (this.categories = response.data))
-        .catch((error) => {
-          console.log(error);
-        });
+    submit() {
+      const formData = new FormData();
+      formData.append("title", this.fields.title);
+      formData.append("category_id", this.fields.category_id);
+      formData.append("body", this.fields.body);
+      formData.append("file", this.fields.file);
+
+      axios.post("/api/posts", formData).then(() => {
+        this.fields = {};
+        this.url = null;
+        this.fields.category_id = "";
+        this.success = true;
+        this.errors = {};
+        setTimeout(() => {
+          this.success = false;
+        }, 2500);
+      }).catch((error) => {
+        this.errors = error.response.data.errors;
+        this.success = false;
+      });
     },
-  };
-  </script>
+  },
+  mounted() {
+    axios.get("/api/categories").then((response) => {
+      this.categories = response.data;
+    }).catch((error) => {
+      console.log(error);
+    });
+  },
+};
+</script>
 
   <style scoped>
   .create-post {
